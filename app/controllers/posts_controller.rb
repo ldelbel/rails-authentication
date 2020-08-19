a699638e3ced98c7e675f23f033e45f754872ddf
ElijahTobs <eligenius@gmail.com>
2020-08-19 13:30:03 +0100

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  
+
   def index
     @posts = Post.all.order('created_at DESC')
     @user = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: [:new, :create, :index, :destroy]
+  resources :posts, only: %i[new create index destroy]
   root 'posts#index'
 end
